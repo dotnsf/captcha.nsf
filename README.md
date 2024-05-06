77bd2ef9f4cb9c43fda65cfeff2717b975110ab5
K.Kimura <dotnsf@gmail.com>
2024-05-06 17:13:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Matchbo-Captcha
+
+
+## Overview
+
+Distributable Matchbo-Captcha.JS
+
+
+## Sample
+
+[Sample](https://dotnsf.github.io/matchbo-captcha/)
+
+
+## Usage
+
+```
+  :
+<div id="matchbo-captcha">
+  <button class="btn">送信</button>
+</div>
+  :
+
+<script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" rel="stylesheet"/>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js"></script>
+<script src="https://dotnsf.github.io/matchbo-captcha/matchbo-captcha.js"></script>
+<script>
+$(function(){
+  var matin = $('#matchbo-captcha').matchbo({});
+});
+
+function sendImage(){
+  :
+}
+
+function submitCanvas(){
+  :
+}
+</script>
+
+```
+
+
+## Licensing
+
+This code is licensed under MIT.
+
+
+## Copyright
+
+2024  [K.Kimura @ Juge.Me](https://github.com/dotnsf) all rights reserved.
---
+++ docs/matchbo-captcha.js
@@ -0,0 +1,113 @@
+//. matchbo-captcha.js
+
+//. Language resources
+var __lang = window.navigator.language;
+//alert( '__lang : ' + __lang );
+if( __lang == 'ja-JP' ){  //. #1
+  __lang = 'ja';
+}
+if( __lang != 'ja' && __lang != 'en' ){
+  __lang = 'en';
+}
+
+var __r = {
+  'ja': {
+    'tool_title': 'マイキャプチャ',
+
+    'dummy': 'ダミー'
+  },
+  'en': {
+    'tool_title': 'My Captcha',
+
+    'dummy': 'Dummy'
+  }
+};
+
+var __THIS = null;
+var __OPTION = null;
+
+var __base_url = location.origin + '/';
+
+$.fn.matchbo = function( option ){
+  __THIS = this;
+  __OPTION = option;
+
+  //. lang
+  if( !__OPTION || !__OPTION.lang ){ __OPTION.lang = __lang; }
+
+  //. uuid
+  if( !__OPTION || !__OPTION.uuid ){ __OPTION.uuid = __generateUUID(); }
+
+  __init();
+
+  return __THIS;
+};
+
+function __init(){
+  var __html = __THIS.html();
+  var new_html = '<div id="__mycaptcha__">'
+    + '<h2>' + __r[__OPTION.lang].tool_title + '</h2>'
+    + '</div><div id="__original_html" class="hide_first__">'
+    + __html
+    + '</div>';
+  __THIS.html( new_html );
+
+  //. プロトタイプを定義
+  __definePrototype();
+
+  //. リサイズ時に Canvas サイズを変更する
+  $(window).on( 'load resize', function(){
+  });
+}
+
+
+function __definePrototype(){
+  //. プロトタイプ関数
+
+};
+
+//. 内部関数
+function __validateAnswer(){
+};
+
+function __isAndroid(){
+  return ( navigator.userAgent.indexOf( 'Android' ) > 0 );
+};
+
+function __timestamp2datetime( __ts ){
+  if( __ts ){
+    var __dt = new Date( __ts );
+    var __yyyy = __dt.getFullYear();
+    var __mm = __dt.getMonth() + 1;
+    var __dd = __dt.getDate();
+    var __hh = __dt.getHours();
+    var __nn = __dt.getMinutes();
+    var __ss = __dt.getSeconds();
+    var __datetime = __yyyy + '-' + ( __mm < 10 ? '0' : '' ) + __mm + '-' + ( __dd < 10 ? '0' : '' ) + __dd
+      + ' ' + ( __hh < 10 ? '0' : '' ) + __hh + ':' + ( __nn < 10 ? '0' : '' ) + __nn + ':' + ( __ss < 10 ? '0' : '' ) + __ss;
+    return __datetime;
+  }else{
+    return "";
+  }
+};
+
+function __generateUUID(){
+  //. Cookie の値を調べて、有効ならその値で、空だった場合は生成する
+  var __did = null;
+  __cookies = document.cookie.split(";");
+  for( var __i = 0; __i < __cookies.length; __i ++ ){
+    var __str = __cookies[__i].split("=");
+    if( unescape( __str[0] ) == " deviceid" ){
+      __did = unescape( unescape( __str[1] ) );
+    }
+  }
+
+  if( __did == null ){
+    var __s = 1000;
+    __did = ( new Date().getTime().toString(16) ) + Math.floor( __s * Math.random() ).toString(16);
+  }
+  var __maxage = 60 * 60 * 24 * 365 * 100; //. 100years
+  document.cookie = ( "deviceid=" + __did + '; max-age=' + __maxage );
+
+  return __did;
+};
